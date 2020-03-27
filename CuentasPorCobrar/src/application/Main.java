32d2f1530ac15ec7c914c6b58d3a42ec80d04112
Samuel Satizabal <samuelsatizabal@hotmail.com>
2020-03-27 11:10:01 -0500

    adding first version of the graphic interface

--- CuentasPorCobrar/src/application/Main.java
+++ CuentasPorCobrar/src/application/Main.java
@@ -1,26 +1,30 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.fxml.FXMLLoader;
-
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Pane root = (Pane) FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root, 1000, 600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("");
+//			primaryStage.getIcons().add(new Image("file:data/logo.jpg"));
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
+
+		} catch (Exception e) {
+			e.printStackTrace();	
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
--- CuentasPorCobrar/src/application/SampleController.java
+++ CuentasPorCobrar/src/application/SampleController.java
@@ -1,5 +1,55 @@
 package application;
 
 public class SampleController {
+	public void initialize() {
+		
+	}
 	
+	public void updateAccount() {
+		
+	}
+	
+	public void createAccount() {
+		
+	}
+	
+	public void searchAccount() {
+		
+	}
+	
+	public void showOutdatedAccounts() {
+		
+	}
+	
+	public void showNoPaidAccounts() {
+		
+	}
+	
+	public void sortAccountsByDueDate() {
+		
+	}
+	
+	public void sortAccountsByType() {
+		
+	}
+	
+	public void showBills() {
+		
+	}
+	
+	public void showClients() {
+		
+	}
+	
+	public void registerClient() {
+		
+	}
+	
+	public void updateClient() {
+		
+	}
+	
+	public void searchClient() {
+		
+	}
 }
