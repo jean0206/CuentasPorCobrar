5c4f1aef4d4db1e6e9310d869eed09cc03fb84b2
jean0206 <jeanortiz00@gmail.com>
2020-05-06 09:36:02 -0500

    HashTable was added

---
+++ CuentasPorCobrar/src/modelo/AccountsReceivableApp.java
@@ -0,0 +1,17 @@
+package modelo;
+
+import java.util.Hashtable;
+
+public class AccountsReceivableApp {
+	
+	private Hashtable<String,Client> clients;
+	
+	public AccountsReceivableApp() {
+		
+		clients= new Hashtable<String,Client>();
+	}
+	
+	
+	
+
+}
